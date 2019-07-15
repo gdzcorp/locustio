0faacd317c798a3a5f8cbd55dd6eab0c704d706f
Grzegorz Dziwoki <grzegorz.dziwoki@enginiety.com>
2019-07-15 11:36:15 +0200

    check if mster enabled in worker mode

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,10 @@ LOCUST_MODE=${LOCUST_MODE:-standalone}
 if [[ "$LOCUST_MODE" = "master" ]]; then
     LOCUST_OPTS="$LOCUST_OPTS --master"
 elif [[ "$LOCUST_MODE" = "worker" ]]; then
+    if [ -z "${LOCUST_MASTER}" ]; then
+        echo "ERROR: LOCUST_MASTER is empty. Worker mode requires a master" >&2
+        exit 1
+    fi
     LOCUST_OPTS="$LOCUST_OPTS --slave --master-host=$LOCUST_MASTER"
 fi
 
